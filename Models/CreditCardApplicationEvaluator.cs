06db27082486704c5e5b77bdce7905a272485a1c
ainaajidian <ainaajidian@gmail.com>
2021-12-28 16:26:07 +0700

    Add project files.

---
+++ Models/CreditCardApplicationEvaluator.cs
@@ -0,0 +1,40 @@
+﻿using SampleUntiTestingApp.Models;
+
+namespace SampleUnitTestingApp.Models
+{
+    public class CreditCardApplicationEvaluator
+    {
+        private IFrequentFlyerNumberValidator _validator;
+
+        private const int AutoReferralMaxAge = 20;
+        private const int HighIncomeThreshold = 100_000;
+        private const int LowIncomeThreshold = 20_000;
+
+        public CreditCardApplicationEvaluator(IFrequentFlyerNumberValidator validator)
+        {
+            _validator = validator;
+        }
+
+        public CreditCardApplicationDecision Evaluate(CreditCardApplication application)
+        {
+            if (!_validator.IsValid(application.FrequentFlyerNumber))
+            {
+                return CreditCardApplicationDecision.ReferredToHuman;
+            }
+
+            if (application.GrossAnnualIncome >= HighIncomeThreshold)
+            {
+                return CreditCardApplicationDecision.AutoAccepted;
+            }
+            if (application.Age <= AutoReferralMaxAge)
+            {
+                return CreditCardApplicationDecision.ReferredToHuman;
+            }
+            if (application.GrossAnnualIncome < LowIncomeThreshold)
+            {
+                return CreditCardApplicationDecision.AutoDeclined;
+            }
+            return CreditCardApplicationDecision.ReferredToHuman;
+        }
+    }
+}
